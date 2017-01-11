c8f3bb7d8ad3a3d8b99b98e773aadc364b4bd5be
Kevin Bayes <kevinbayes@Admin-088s-MacBook-Pro.local>
2017-01-12 08:16:46 +1300

    Initial

---
+++ configclient-net/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Steeltoe.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            IHostingEnvironment env = new HostingEnvironment();
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()                   
+            .AddConfigServer(env);
+
+            var config = builder.Build();
+
+            
+            Console.WriteLine($"{config["message"]}");
+
+        }
+    }
+}
---
+++ configclient/src/main/java/io/plexure/config/client/ConfigClientApplication.java
@@ -0,0 +1,38 @@
+package io.plexure.config.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+
+	@RefreshScope
+	@RestController("/api")
+	public static class SampleController {
+
+		public String message;
+
+		@Autowired
+		public SampleController(@Value("${message}") String message) {
+			this.message = message;
+		}
+
+		@RequestMapping(value = "/api/message", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+		public String api() {
+
+			return this.message;
+		}
+	}
+}
